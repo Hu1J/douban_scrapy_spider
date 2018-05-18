a77219487c102966b96c1da51c5113e654efb48c
Guixuan <2948696442@qq.com>
2018-05-18 19:46:14 +0800

    Change images path to absolute path

--- doubanbook/Spider_GUI/MainWin.py
+++ doubanbook/Spider_GUI/MainWin.py
@@ -19,7 +19,7 @@ class Ui_MainWindow(object):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("imgs/heart.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap("/home/guixuan/Documents/py/Python-spyder/doubanbook/doubanbook/Spider_GUI/imgs/heart.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         MainWindow.setWindowIcon(icon)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
